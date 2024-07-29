abb19f26647f076b743ec43699afb72760b0e80f
User <user@Users-MacBook-Pro.local>
2024-07-29 20:06:16 +0100

    adding project state to manage selected projects or not selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectsId: undefined,
+    projects: []
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectsId: null
+      };
+    });
+  }
+  //process of adding a new project
+
+  let content;
+
+  if(projectsState.selectedProjectsId === null) {
+    content = <NewProject />;
+  }
+  else if(projectsState.selectedProjectsId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
